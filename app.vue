8eb34002b0ce83ddc545eebdb94766b8c6ed8857
Vi Ly <vi.lh@>
2025-05-06 10:48:42 +0700

    learn table display mode

--- app.vue
+++ app.vue
@@ -1,8 +1,14 @@
 <template>
   <UApp> <!-- UApp for NextUI showToast -->
-    <div  class="flex justify-between gap-2 p-4 border m-[10px] " > 
-      <div class="font-bold">
-        CRM and Logo Here
+    <div  class="flex justify-between gap-2 p-4 border m-[10px]  items-center" > 
+      <div class="flex items-center justify-center gap-2 font-bold"> 
+        <span>
+          <a href="/"> 
+            <img src="@/assets/images/logo.jpeg" style="border-radius: 100%; width: 50px" alt="logo"/>
+          </a>
+        </span>
+        <span>  <a href="/">  Company's name </a> </span>
+       
       </div>
       <div class="flex gap-2">
         <div>
@@ -16,7 +22,7 @@
     </div>
 
     <div class="flex gap-2" > 
-      <div>
+      <div class="hidden md:block"> <!-- binh thuong là ẩn, từ md: 768 trở lên thì hiển thị hết -->
         <NavigationMenu />  
       </div>
       <div class="grow">
